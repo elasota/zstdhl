17b26dd5530a43399c1428134d48effdc0c03180
elasota <1137273+elasota@users.noreply.github.com>
2024-01-07 20:12:44 -0500

    Update to MIT/Apache dual license

--- zstdhl_util.h
+++ zstdhl_util.h
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2023 Eric Lasota
+
+This software is available under the terms of the MIT license
+or the Apache License, Version 2.0.  For more information, see
+the included LICENSE.txt file.
+*/
 
 #define ZSTDHL_CHECKED(n) do {\
 		zstdhl_ResultCode_t result = (n);\
